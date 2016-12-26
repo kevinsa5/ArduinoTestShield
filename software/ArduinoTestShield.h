e63d28a4b6877945c1c8cb9c1817266d185d52fe
Kevin Anderson <kevinanderso@gmail.com>
2016-12-26 16:43:47 -0500

    fixed symlink for ArduinoTestShield.h

--- software/ArduinoTestShield.h
+++ software/ArduinoTestShield.h
@@ -1,1 +1,11 @@
-/home/kevin/Arduino/libraries/FrugalinoTests/FrugalinoTests.h
+const int numTests = 10;
+void (*methodPtrs[numTests])();
+String testIDs[numTests];
+
+void digitalReadTest();
+void digitalWriteTest();
+
+void initializeTestArrays(){
+    testIDs[0] = "DRT"; methodPtrs[0] = digitalReadTest;
+    testIDs[1] = "DWT"; methodPtrs[1] = digitalWriteTest;
+}
